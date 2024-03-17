e65b522999728d32fc29977440a5816d0011b608
pklata <piotrek.klata@gmail.com>
2024-03-17 10:55:10 +0100

    commit on 'master'

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ models/song.js
@@ -0,0 +1,7 @@
+
+const songs = [
+    { id: 1, title: 'Song 1', duration: '3:45' },
+    { id: 2, title: 'Song 2', duration: '5:00' }
+];
+
+module.exports = songs;
