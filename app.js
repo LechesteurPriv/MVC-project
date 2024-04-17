9e2e7bbd45aa483838b2b881bb07cac6451933b0
Kamil Cessak <kamil.cessak@mindconsulting.pl>
2024-04-17 22:48:32 +0200

    added some config

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 const PORT = process.env.PORT || 3000;
