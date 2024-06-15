521e1600b1e0719c084be056416f7ee95ff0101a
pklata <piotrek.klata@gmail.com>
2024-06-15 13:03:38 +0200

    nowa baza

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  "freedb_music_player_db",
-  "freedb_muser",
-  "72!Ax3NNSRv49aD",
+  //"freedb_music_player_db",
+    "defaultdb",
+  //"freedb_muser",
+    "avnadmin",
+  //"72!Ax3NNSRv49aD",
+    "AVNS_wyaqKSaitVa2yo1rMoP",
   {
-    host: "sql.freedb.tech",
+   // host: "sql.freedb.tech",
+    host: "sql7714081-piotrek-1ee6.g.aivencloud.com",
     dialect: "mysql",
-    port: 3306,
+    port: 25972,
     logging: false,
   }
 );
